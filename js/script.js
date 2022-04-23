1311cf68e8409fde57bf0fadf418cb548d968d4c
hideki watabe <hideki.w.1971@gmail.com>
2022-04-24 00:39:16 +0900

    2022/4/24 完成(menu-list が一つのケース)

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
 
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
+  $(".MenuButton").click(function () {
     // $(".l-drawer-menu").toggleClass("is-show");
     // $(".p-drawer-menu").toggleClass("is-show");
     // $(".js-drawer-open").toggleClass("open");
-    $("#MenuButton").toggleClass("open");
-    // $(".drawer-menu").toggleClass("open");
+    $(".MenuButton").toggleClass("open");
+    $(".drawer-menu").toggleClass("open");
     // $("html").toggleClass("is-fixed");
   });
 });
